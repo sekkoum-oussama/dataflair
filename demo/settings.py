80835188a093e42cf5c31b67cb0519e263c67305
sekkoum-oussama <69439465+sekkoum-oussama@users.noreply.github.com>
2020-09-09 22:34:54 +0100

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -77,13 +77,9 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'dataflair',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
